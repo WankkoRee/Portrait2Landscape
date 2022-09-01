b320438ff63c1dd77c4fa0c59eeb15eaf7308407
Wankko Ree <wkr@wkr.moe>
2022-09-01 20:22:48 +0800

    feat: 添加对国际版的适配

---
+++ app/src/main/java/cn/wankkoree/xposed/portrait2landscape/hook/Hook.kt
@@ -0,0 +1,47 @@
+package cn.wankkoree.xposed.portrait2landscape.hook
+
+import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
+import com.highcapable.yukihookapi.hook.log.loggerD
+import com.highcapable.yukihookapi.hook.type.java.StringType
+
+class Hook: YukiBaseHooker() {
+    override fun onHook() {
+        "com.bilibili.pegasus.api.model.BasicIndexItem".hook {
+            injectMember {
+                allConstructors()
+                afterHook {
+                    val uri = field {
+                        name = "uri"
+                        type = StringType
+                    }.get(instance)
+                    uri.string().let {
+                        if (it.isNotEmpty()) {
+//                                loggerD(msg = "BasicIndexItem.<init>(url = $it)")
+                            if (it.startsWith("bilibili://story/")) {
+                                uri.set("bilibili://video/" + it.substringAfter("bilibili://story/"))
+                                loggerD(msg = "初始化视频信息时，竖屏转横屏成功")
+                            }
+                        }
+                    }
+                }
+            }
+            injectMember {
+                method {
+                    name = "setUri"
+                    param(StringType)
+                }
+                beforeHook {
+                    (args[0]!! as String).let {
+                        if (it.isNotEmpty()) {
+//                                loggerD(msg = "BasicIndexItem.setUri(url = $it)")
+                            if (it.startsWith("bilibili://story/")) {
+                                args(0).set("bilibili://video/" + it.substringAfter("bilibili://story/"))
+                                loggerD(msg = "设定视频链接时，竖屏转横屏成功")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/cn/wankkoree/xposed/portrait2landscape/hook/Main.kt
+++ app/src/main/java/cn/wankkoree/xposed/portrait2landscape/hook/Main.kt
@@ -2,8 +2,6 @@ package cn.wankkoree.xposed.portrait2landscape.hook
 
 import com.highcapable.yukihookapi.YukiHookAPI
 import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
-import com.highcapable.yukihookapi.hook.log.loggerD
-import com.highcapable.yukihookapi.hook.type.java.StringType
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 
 @InjectYukiHookWithXposed(entryClassName = "Entry", isUsingResourcesHook = false)
@@ -20,44 +18,8 @@ class Main : IYukiHookXposedInit {
     }
 
     override fun onHook() = YukiHookAPI.encase {
-        loadApp("tv.danmaku.bili") {
-            "com.bilibili.pegasus.api.model.BasicIndexItem".hook {
-                injectMember {
-                    allConstructors()
-                    afterHook {
-                        val uri = field {
-                            name = "uri"
-                            type = StringType
-                        }.get(instance)
-                        uri.string().let {
-                            if (it.isNotEmpty()) {
-//                                loggerD(msg = "BasicIndexItem.<init>(url = $it)")
-                                if (it.startsWith("bilibili://story/")) {
-                                    uri.set("bilibili://video/" + it.substringAfter("bilibili://story/"))
-                                    loggerD(msg = "初始化视频信息时，竖屏转横屏成功")
-                                }
-                            }
-                        }
-                    }
-                }
-                injectMember {
-                    method {
-                        name = "setUri"
-                        param(StringType)
-                    }
-                    beforeHook {
-                        (args[0]!! as String).let {
-                            if (it.isNotEmpty()) {
-//                                loggerD(msg = "BasicIndexItem.setUri(url = $it)")
-                                if (it.startsWith("bilibili://story/")) {
-                                    args(0).set("bilibili://video/" + it.substringAfter("bilibili://story/"))
-                                    loggerD(msg = "设定视频链接时，竖屏转横屏成功")
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        val hooker = Hook()
+        loadApp("tv.danmaku.bili", hooker)
+        loadApp("com.bilibili.app.in", hooker)
     }
 }
